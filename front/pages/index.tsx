149ee6ddf3def0046f43b63084f15c92da231553
Oleg0110 <oleg.buno13@gmail.com>
2022-05-17 14:03:03 +0300

    Start

---
+++ front/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+
+const Index = () => {
+  return (
+    <div>
+      Start
+      <Button>DA</Button>
+      <PersonIcon style={{ color: '685248' }} />
+    </div>
+  );
+};
+
+export default Index;
